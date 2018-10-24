1c2ae483be49955c6eab2eb601c63d8c966c1ae4
przemo <przemo2@gmail.com>
2018-10-24 18:54:31 +0200

    init+ poprawki

--- src/WriteListToLatex.java
+++ src/WriteListToLatex.java
@@ -15,32 +15,53 @@ public class WriteListToLatex {
             fileWriter = new FileWriter("plik.tex");
             bfw = new BufferedWriter(fileWriter);
 
-            temp=(ArrayList)list.get(1);
-            size=temp.size();
-            size2=list.size();
+            temp = (ArrayList) list.get(1);
+            size = temp.size();
+            size2 = list.size();
 
-            bfw.write("\\begin{tabular}{ ");
-            for (int i = 0; i < size ; i++) {
-                bfw.write("c ");
+            bfw.write("\\begin{table}\n");
+            bfw.write("\\begin{center}\n");
+            bfw.write("\\begin{tabular}{");
+            for (int i = 0; i < size; i++) {
+
+                bfw.write("| c ");
             }
-            bfw.write("}\n");
+            bfw.write("|}\n");
+            bfw.write("\\hline\n");
 
             for (int i = 0; i < list.size(); i++) {
 
+                if(i%2 !=0){
+                    bfw.write("\\rowcolor{light-gray}\n");
+                }
+
                 ArrayList innerList = (ArrayList) list.get(i);
                 for (int ii = 0; ii < innerList.size(); ii++) {
-                    String cell= (String)innerList.get(ii);
+                    String cell = (String) innerList.get(ii);
+                    if (i == 0){
+                    bfw.write("\\textbf{" + cell + "}");
+                    }else
                     bfw.write(cell);
-                    if (ii == innerList.size()-1 && i!=size2-1){
+
+                    if (ii == innerList.size() - 1) {
                         bfw.write(" \\\\");
-                    }else if (ii != innerList.size()-1)
+                    } else if (ii != innerList.size() - 1)
                         bfw.write(" & ");
                 }
+                bfw.write("\n\\hline");
+                if (i == 0)
+                    bfw.write(" \\hline");
+
                 bfw.write("\n");
 
             }
-bfw.write("\\end{tabular}\n");
+            bfw.write("\\end{tabular}\n");
+            bfw.write("\\end{center}\n");
+            bfw.write("\\caption{tab1}\n");
 
+            bfw.write("\\label{tab:t1}\n");
+
+            bfw.write("\\end{table}\n");
             bfw.close();
         } catch (IOException e) {
             System.out.println("plik nie zapisany");
